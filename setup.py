7b6c68a473613f21db17d81455ffae6fbab53fb6
Kim Hermansson <h4xrk1m@gmail.com>
2018-08-30 19:18:53 +0200

    - Changed the version number.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='statepattern',
 
-    version='1.0.0',
+    version='0.1.0',
 
     description='Object used to maintain state.',
     long_description=long_description,
